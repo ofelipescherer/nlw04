ddac22d83236a19c3992b9554e0ed3173043735a
unknown <felipescherer25@gmail.com>
2021-02-27 10:43:56 -0300

    fix challengesCompleted propriety

--- src/components/CompleteChallanges.tsx
+++ src/components/CompleteChallanges.tsx
@@ -3,12 +3,12 @@ import { ChallengeContext } from '../contexts/ChallangesContext';
 import styles from '../styles/components/CompleteChallanges.module.css'
 
 export function CompleteChallanges(){
-    const { ChallengesCompleted } = useContext(ChallengeContext);
+    const { completeChallenge } = useContext(ChallengeContext);
 
     return (
         <div className={styles.completeChallangesContainer}>
             <span>Desafios Completos</span>
-            <span>{ChallengesCompleted}</span>
+            <span>{completeChallenge}</span>
         </div>
     );
 }
